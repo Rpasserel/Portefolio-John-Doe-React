b54f62526629d9ab528dcee60958afa0ee06912a
Passerel Rémi <remipasserel@gmail.com>
2026-04-09 20:13:22 +0200

    amélioration de la NavBar + implémentation de React Router et ajout de CSS dans la NavBar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,28 +2,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
 
+import './assets/style/Navbar.css'
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">JOHN DOE</Navbar.Brand>
+      <Container className="NavBar">
+        <div></div>
+        <Navbar.Brand href="#home" className="linkHome">JOHN DOE</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Accueil</Nav.Link>
-            <Nav.Link href="#link">Mes services</Nav.Link>
-            <NavDropdown title="Mes réalisations" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Réalisation 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Réalisation 2
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Réalisation 3</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Contact
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={NavLink} to="/Home" className="link">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/Services" className="link">Services</Nav.Link>
+            <Nav.Link as={NavLink} to="/Portefolio" className="link">Portefolio</Nav.Link>
+            <Nav.Link as={NavLink} to="/Contact" className="link">Contact</Nav.Link>
+            <Nav.Link as={NavLink} to="/MentionsLegales" className="link">Mentions Légales</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
